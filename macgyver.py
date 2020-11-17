e0acf7ea5bf4373ac3d01a7995ad116fa8452cf2
ihsan <ihsan.salmantr@gmail.com>
2020-11-17 14:27:19 +0000

    modif

--- macgyver.py
+++ macgyver.py
@@ -18,27 +18,28 @@ class Macgyver:
 		
 	def move_right(self):
 		# Checking two conditions to be sure that the movement is possible
-		if ((self.pos_x) <= (constants.NB_SPRITE - 1)) and (self.labyrinth.structure[self.pos_y][self.pos_x + 1] != 'm'):
+		if ((self.pos_x) < (constants.NB_SPRITE - 1)) and (self.labyrinth.structure[self.pos_y][self.pos_x + 1] != 'm'):
 			self.pos_x += 1
 			self.display()
-			print('you choose to move to the right')
+			print('you choose to move to the right', self.pos_x, self.pos_y)
 			
 			
 	def move_left(self):
-		if (self.pos_x > 0) and (self.lab.structure[self.pos_x - 1][self.pos_y] != 'm'):
-				self.pos_x -= 1
-				print('you choose to move to the left')
+		if (self.pos_x > 0) and (self.labyrinth.structure[self.pos_y][self.pos_x - 1] != 'm'):
+			self.pos_x -= 1
+			self.display()
+			print('you choose to move to the left', self.pos_x, self.pos_y)
 	
 	
 	def move_up(self):
-		if self.pos_y < (constants.NB_SPRITE - 1):
-			if self.lab.structure[self.pos_x][self.pos_y + 1] != 'm':
-				self.pos_y += 1
-				print('you choose to move to the top')
+		if (self.pos_y > 0) and (self.labyrinth.structure[self.pos_y - 1][self.pos_x] != 'm'):
+			self.pos_y -= 1
+			self.display()
+			print('you choose to move to the top', self.pos_x, self.pos_y)
 			
 			
 	def move_down(self):
-		if self.pos_y > 0:
-			if self.lab.structure[self.pos_x][self.pos_y - 1] != 'm':
-				self.pos_y -= 1
-				print('you choose to move down')
+		if (self.pos_y < (constants.NB_SPRITE - 1)) and (self.labyrinth.structure[self.pos_y + 1][self.pos_x] != 'm'):
+				self.pos_y += 1
+				self.display()
+				print('you choose to move down', self.pos_x, self.pos_y)
--- main.py
+++ main.py
@@ -27,6 +27,7 @@ screen.blit(image_font, (0, 0))
 # Instances of classes
 lab = Labyrinth(screen)
 mac = Macgyver(lab)
+
 while run:
 
 	# Player action that allows interaction with the game
@@ -39,14 +40,12 @@ while run:
 		if e.type == pygame.KEYDOWN:
 			if e.key == pygame.K_RIGHT :
 				mac.move_right()
-
-
-		
+			if e.key == pygame.K_LEFT :
+				mac.move_left()
+			if e.key == pygame.K_UP :
+				mac.move_up()
+			if e.key == pygame.K_DOWN :
+				mac.move_down()
+			
 	
 	pygame.display.flip()
-	
-
-       
-			
-				
-       
