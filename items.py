031407e0a88de0671264931d614bb92465e068dc
ihsan <ihsan.salmantr@gmail.com>
2020-11-24 12:39:56 +0000

    items class redaction

--- items.py
+++ items.py
@@ -1,30 +1,37 @@
 # importation of moduls
 import pygame 
+from random import randint
 import constants as constants
 
 class Items:
 
 	def __init__(self, labyrinth):
 		self.labyrinth = labyrinth
+		self.x = 0
+		self.y = 0
+		self.pos_item = []
 		self.needle = pygame.image.load(constants.IMAGE_NEEDLE).convert_alpha()
 		self.ether = pygame.image.load(constants.IMAGE_ETHER).convert_alpha()
 		self.syringe = pygame.image.load(constants.IMAGE_SYRINGE).convert_alpha()
-
-		def pos_items(self):
-			self.items_pos = []
-			for x in range(len(self.structure)):
-				for y in range(len(self.structure[x])):
-					element = self.structure[y][x]
-					if element == 'o':
-						position_items = y, x
-						self.items_pos.append(position_items)
-						self.pos_items = random.sample(self.items_pos, 3)
+		self.display()
 
 
-		def display(self):
-			needle_pos, syringe_pos, ether_pos = self.pos_items
-			self.screen.blit(self.needle, (needle_pos[0] * constants.SPRITE_SIZE, needle_pos[1] * constants.SPRITE_SIZE))
-			self.screen.blit(self.syringe, (syringe_pos[0] * constants.SPRITE_SIZE, syringe_pos[1] * constants.SPRITE_SIZE))
-			self.screen.blit(self.ether, (ether_pos[0] * constants.SPRITE_SIZE, ether_pos[1] * constants.SPRITE_SIZE))
+	def item_pos(self):
+		while self.pos_item == []:
+			self.x = randint(0, 14)
+			self.y = randint(0, 14)
+			if self.labyrinth.structure[self.y][self.x] != 'm':
+				self.pos_item.append(self.y)
+				self.pos_item.append(self.x)
+		return self.pos_item
 
+	def display(self):
+		needle_pos = self.item_pos()
+		print(needle_pos)
+		syringe_pos = self.item_pos()
+		print(syringe_pos)
+		self.labyrinth.screen.blit(self.needle, (needle_pos[1] * constants.SPRITE_SIZE, needle_pos[0] * constants.SPRITE_SIZE))
+		"""self.labyrinth.screen.blit(self.syringe, (syringe_pos[1] * constants.SPRITE_SIZE, syringe_pos[0] * constants.SPRITE_SIZE))
+		self.labyrinth.screen.blit(self.ether, (ether_pos[0] * constants.SPRITE_SIZE, ether_pos[1] * constants.SPRITE_SIZE))
+        """
 
